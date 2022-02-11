13dc83bddcd7c49b03057e6290aaf2964ec517ac
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-02-11 22:45:35 +0200

    UI elements added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Row, Container, Col } from 'reactstrap';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [result, setResult] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <div id="display">{result}</div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button id="zero">0</Button>
+          </Col>
+          <Col>
+            <Button id="one">1</Button>
+          </Col>
+          <Col>
+            <Button id="two">2</Button>
+          </Col>
+          <Col>
+            <Button id="three">3</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button id="four">4</Button>
+          </Col>
+          <Col>
+            <Button id="five">5</Button>
+          </Col>
+          <Col>
+            <Button id="six">6</Button>
+          </Col>
+          <Col>
+            <Button id="seven">7</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button id="eight">8</Button>
+          </Col>
+          <Col>
+            <Button id="nine">9</Button>
+          </Col>
+          <Col>
+            <Button id="decimal">.</Button>
+          </Col>
+          <Col>
+            <Button id="clear">C</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button id="add">+</Button>
+          </Col>
+          <Col>
+            <Button id="subtract">-</Button>
+          </Col>
+          <Col>
+            <Button id="multiply">*</Button>
+          </Col>
+          <Col>
+            <Button id="divide">/</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button id="equals">=</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
